93b98c2c18f05290b06c91c4f6c37f8f2db54b09
Day <dayannefernandesc@gmail.com>
2017-06-03 23:09:04 -0300

    test flask

---
+++ src/main.py
@@ -0,0 +1,14 @@
+# from selenium import webdriver
+from flask import Flask
+
+local_app = Flask('local_app')
+
+# def main():
+#     # create the fake browser
+#     driver = webdriver.Firefox()
+#
+#     # get request using the fake browser
+#     driver.get('/')
+#
+#     # close the fake browser
+#     driver.quit()
