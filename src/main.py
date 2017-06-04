9ff8bbb80eb81817c12de98e90e0d821321cafdd
Day <dayannefernandesc@gmail.com>
2017-06-04 00:03:56 -0300

    basic forms test and implementation

--- src/main.py
+++ src/main.py
@@ -5,7 +5,11 @@ local_app = Flask('local_app')
 
 @local_app.route('/')
 def home():
-    return render_template('about.html')
+    return render_template('index.html')
+
+@local_app.route('/login')
+def login():
+    return render_template('forms.html')
 
 if __name__ == '__main__':
     local_app.run()
--- src/tests.py
+++ src/tests.py
@@ -6,7 +6,8 @@ class TestHome(unittest.TestCase):
     def setUp(self):
         app = local_app.test_client()
         self.response = app.get('/')
-        self.response_str = self.response.data.decode('utf-8')
+        # return bytes
+        self.content = self.response.data.decode('utf-8')
 
     def test_get_status_code(self):
         self.assertEqual(200, self.response.status_code)
@@ -15,13 +16,29 @@ class TestHome(unittest.TestCase):
         self.assertIn('text/html', self.response.content_type)
 
     def test_index_title(self):
-        self.assertIn('<title>Dojo Day 2017</title>', str(self.response_str))
+        self.assertIn('<title>Dojo Day 2017</title>', self.content)
 
     def test_index_event_detail(self):
-        self.assertIn('<p>Como testar sua', str(self.response_str))
+        self.assertIn('<p>Como testar sua', self.content)
 
     def test_bootstrap_css(self):
-        self.assertIn('bootstrap.min.css', self.response_str)
+        self.assertIn('bootstrap.min.css', self.content)
+
+class TestForms(unittest.TestCase):
+
+    def setUp(self):
+        app = local_app.test_client()
+        self.response = app.get('/login')
+        self.content = self.response.data.decode('utf-8')
+
+    def test_login_form(self):
+        self.assertIn('type="email"', self.content)
+
+    def test_password_form(self):
+        self.assertIn('type="password"', self.content)
+
+    def test_send_button(self):
+        self.assertIn('type="submit"', self.content)
 
 if __name__ == '__main__':
     unittest.main()
